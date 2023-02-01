3db7834bc0da299fb0833fcc2043f4aed84c103d
ceydaulubas <ceyda.ulubas@gmail.com>
2023-02-01 13:00:17 +0300

    narbar created by styled component

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,31 +1,44 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import { Calendar2Check } from 'react-bootstrap-icons';
-
+import { NavigationContainer, NavInnerContainer, LeftContainer, NavLinkContainer } from './NavBar.sc';
 const NavBar = () => {
-  return (
-    <Navbar collapseOnSelect expand='lg' bg='dark' variant='dark'>
-      <Container>
-        <Navbar.Brand href='/'>
-          Planner
-          <Calendar2Check color='white' style={{ marginLeft: '10px' }} />
-        </Navbar.Brand>
+  const [extendNavbar, setExtendNavbar] = useState(false);
 
-        <Navbar.Collapse id='responsive-navbar-nav'>
-          <Nav className='me-auto'>
-            <Nav.Link href='/ToDoList'>To Do List</Nav.Link>
-            <Nav.Link href='/Calender'>Calender</Nav.Link>
-            <Nav.Link href='/Success'>Success</Nav.Link>
-          </Nav>
-          <Nav>
-            <Nav.Link href='/Profil'>Profil</Nav.Link>
-            <Nav.Link href='#logout'>Log Out</Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
+  const closeExtended = () => {
+    setExtendNavbar(false);
+  };
+
+  return (
+    // <Navbar collapseOnSelect expand='lg' bg='dark' variant='dark'>
+    //   <Container>
+    //     <Navbar.Brand href='/'>
+    //       Planner
+    //       <Calendar2Check color='white' style={{ marginLeft: '10px' }} />
+    //     </Navbar.Brand>
+
+    //     <Navbar.Collapse id='responsive-navbar-nav'>
+    //       <Nav className='me-auto'>
+    //         <Nav.Link href='/ToDoList'>To Do List</Nav.Link>
+    //         <Nav.Link href='/Calender'>Calender</Nav.Link>
+    //         <Nav.Link href='/Success'>Success</Nav.Link>
+    //       </Nav>
+    //       <Nav>
+    //         <Nav.Link href='/Profil'>Profil</Nav.Link>
+    //         <Nav.Link href='#logout'>Log Out</Nav.Link>
+    //       </Nav>
+    //     </Navbar.Collapse>
+    //   </Container>
+    // </Navbar>
+    <NavigationContainer extendNavbar={extendNavbar}>
+      <NavInnerContainer>
+        <LeftContainer>
+          <NavLinkContainer></NavLinkContainer>
+        </LeftContainer>
+      </NavInnerContainer>
+    </NavigationContainer>
   );
 };
 
---
+++ src/components/NavBar/NavBar.sc.js
@@ -0,0 +1,21 @@
+import styled from 'styled-components';
+import { Link } from 'react-router-dom';
+
+export const NavigationContainer = styled.nav`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  background-color: red;
+  /* padding-top: 40px;
+  margin-bottom: 100px;
+  @media (min-width: 700px) {
+    height: 80px;
+  } */
+`;
+
+export const NavInnerContainer = styled.div`
+  width: 100%;
+  height: 80px;
+  display: flex;
+`;
+export const LeftContainer = styled.div``;
